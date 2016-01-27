5f113d68854a4dcb225d87ce89ef5218a57c13ba
Mateus Queiroz Correia <mateus_queiroz@hotmail.com>
2016-01-26 22:39:38 -0200

    Webdriver helloworld (Chrome, Firefox)

---
+++ WebDriverHelloWorld/UnitTest1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+
+namespace WebDriverHelloWorld
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //Navegando com o Chrome
+            using (IWebDriver driver = new OpenQA.Selenium.Chrome.ChromeDriver())
+            {
+                SearchAndNavigateTest(driver);
+            }
+
+            //Navegando com o Firefox
+            using (IWebDriver driver = new OpenQA.Selenium.Firefox.FirefoxDriver())
+            {
+                SearchAndNavigateTest(driver);
+            }
+        }
+
+        private static void SearchAndNavigateTest(IWebDriver driver)
+        {
+            try
+            {
+                driver.Navigate().GoToUrl("http://www.bing.com/");
+                driver.Manage().Timeouts().ImplicitlyWait(TimeSpan.FromSeconds(30));
+                IWebElement searchInput = driver.FindElement(By.Id("sb_form_q"));
+                searchInput.SendKeys("aplicacoesweb selenium");
+                searchInput.SendKeys(Keys.Return);
+
+                IWebElement webSiteAnchor = driver.FindElement(By.XPath(("//a[contains(@href,\"aplicacoesweb.blogspot\")]")));
+                webSiteAnchor.Click();
+            }
+            finally
+            {
+                driver.Close();
+            }
+        }
+    }
+}
